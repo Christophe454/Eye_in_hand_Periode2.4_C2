d46f993efa6eb92eb955256e9068f78f7abe16fc
Gerard Harkema <GerardAnneHarkema@gmail.com>
2026-04-21 16:09:04 +0200

    20260420

--- my_ur_bringup/launch/real_robot.launch.py
+++ my_ur_bringup/launch/real_robot.launch.py
@@ -86,6 +86,7 @@ def generate_launch_description():
             description="Activate loaded joint controller.",
         )
     )
+    
 
     # Initialize Arguments
     robot_ip = LaunchConfiguration("robot_ip")
